05bb62f62c2ef75d77913ca21d2e39219e020130
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2022-05-06 22:32:44 +1200

    Add help item

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -15,7 +15,7 @@ const Help = (props) => {
             <Divider />
             <div className="settings__row">
                 <div className="col gap max-width">
-                    <h2>Why can't I see quests and achievements?</h2>
+                    <h2>"Why can't I see my quests and achievements?"</h2>
                     <p>
                         XIV Tracker uses a characters achievements to determine quest 
                         completions. Lodestone has achievements set to private by 
@@ -24,13 +24,22 @@ const Help = (props) => {
                             style={{textDecoration: "underline"}}
                         >here</a>
                     </p>
+                    <h2>"Is XIV Tracker spoiler safe?"</h2>
+                    <p>
+                        By default XIV Tracker will blur out content that could be
+                        considered a spoiler for the main scenario questline. 
+                        You must link a character from Lodestone (using the 
+                        character id on the url) to view spoiler content.
+                    </p>
                 </div>
                 <div className="col gap max-width">
-                    <h2>Wait a minute, that isn't correct!</h2>
+                    <h2>"Wait a minute, where is my mount!?"</h2>
                     <p>
                         XIV Tracker is dependent on resources such as the official 
                         Lodestone site. Loadestone itself updates approximately every 12
-                        hours. XIV Tracker can only show data from the most recent update.
+                        hours. XIV Tracker can only show data from the most recent update,
+                        so XIV Tracker will not display updates to a character from the past 
+                        12 hours.
                     </p>
                 </div>
             </div>
