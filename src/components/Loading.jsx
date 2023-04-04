016a5c33ad940902c15c564183cb47a84dc1882e
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2023-04-05 11:51:32 +1200

    Update loading component to use a sticker

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,20 +1,18 @@
-import loadingIcon from '../images/loading.svg';
-import '../styles/Loading.css';
+import "../styles/Loading.css";
+import titan from "../images/titan.png";
 
 const Loading = (props) => {
-    const {
-        full
-    } = props;
-    return(
-        <div className={'col align-center justify-center gap' + (full ? ' full-page' : '')}>
-            <img 
-                src={loadingIcon} 
-                className='loading__icon'
-                alt="Loading Icon"
-            />
-            <p>Fetching data from Lodestone ...</p>
-        </div>        
-    );
-}
+  const { full } = props;
+  return (
+    <div
+      className={
+        "col align-center justify-center gap-lg" + (full ? " full-page" : "")
+      }
+    >
+      <img src={titan} className="loading__icon" alt="Loading Icon" />
+      <p>Fetching data from Lodestone ...</p>
+    </div>
+  );
+};
 
 export default Loading;
