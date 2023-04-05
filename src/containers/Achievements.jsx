3c3898b7345e41786febb4036dc39300cf49871a
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2023-04-06 11:04:01 +1200

    Add searchbar to achievements

--- src/containers/Achievements.jsx
+++ src/containers/Achievements.jsx
@@ -1,5 +1,5 @@
 // Hooks
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Components
 import Achievement from "../components/Achievement";
@@ -9,7 +9,9 @@ import FailToLoad from "../components/FailToLoad";
 
 // Style
 import "../styles/Achievements.css";
+import { FaSearch } from "react-icons/fa";
 
+const iconSize = "1em";
 /**
  * @name Achievements
  * @description Container for character achievements.
@@ -20,33 +22,52 @@ const Achievements = (props) => {
   const { display, achievements } = props;
   const capacity = 8;
   const [index, setIndex] = useState(0);
+  const [searchInput, setSearchInput] = useState("");
+  const [possibleAchievements, setPossibleAchievements] = useState([]);
+
+  useEffect(() => {
+    setPossibleAchievements(
+      achievements.List.filter((achievement) =>
+        achievement.Name.toLowerCase().includes(searchInput)
+      )
+    );
+    setIndex(0);
+  }, [searchInput]);
 
   return (
     <div className={"section" + (display ? "" : " disabled")}>
       <Header name="Achievements" minor="Points" major={achievements.Points} />
+      <div className="row gap align-center">
+        <input
+          type="text"
+          placeholder="Search"
+          className="collection__searchbar"
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <FaSearch size={iconSize} />
+      </div>
       {achievements.List.length === 0 ? (
         <FailToLoad />
       ) : (
         <>
           <ul className="col gap">
-            {achievements.List.slice(
-              index * capacity,
-              (index + 1) * capacity
-            ).map((achievement) => (
-              <Achievement
-                name={achievement.Name}
-                icon={achievement.Icon}
-                points={achievement.Points}
-                id={achievement.ID}
-                key={achievement.ID}
-              />
-            ))}
+            {possibleAchievements
+              .slice(index * capacity, (index + 1) * capacity)
+              .map((achievement) => (
+                <Achievement
+                  name={achievement.Name}
+                  icon={achievement.Icon}
+                  points={achievement.Points}
+                  id={achievement.ID}
+                  key={achievement.ID}
+                />
+              ))}
           </ul>
           <Navigator
             update={setIndex}
             current={index}
             min={0}
-            max={Math.ceil(achievements.List.length / capacity) - 1}
+            max={Math.ceil(possibleAchievements.length / capacity) - 1}
             style={{ margin: "auto" }}
           />
         </>
